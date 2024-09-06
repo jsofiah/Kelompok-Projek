e9ea84cbfff55a44d3d039bda0e75950a1a6c7a3
nazwazhrr <nazwaazahra883@gmail.com>
2024-09-06 14:34:37 +0700

    commit dari nazwa

---
+++ Biodata15.java
@@ -0,0 +1,19 @@
+public class Biodata15 {
+    
+
+
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------");
+        System.out.println("------------- Biodata Saya -------------");
+        System.out.println("----------------------------------------");
+        System.out.println("Nama     : Nazwa Azahra Audina");
+        System.out.println("Nim      : 244107060146");
+        System.out.println("Kelas    : SIB 1A");
+        System.out.println("No Absen : 15");
+        System.out.println("Prodi    : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat   : Jl.Dasirin No.6 Tlekung");
+        System.out.println("No HP    : 085716431911");
+        System.out.println("Hobi     : Scroll TikTok");
+
+    }   
+}
